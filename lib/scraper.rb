16baa0ec8df153003f731224b521f2137a7cd5af
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-03 18:43:32 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,10 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(html)
 
+p doc.css(".title-3Kf9MY")[1].children
 
+# courses.each do |course|
+#   puts course.text.strip
+# end
