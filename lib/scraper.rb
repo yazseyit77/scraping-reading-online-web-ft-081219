e9df40cefa568af6725d45ea3f6e81d9ab1c7769
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-03 18:35:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
-p doc.css(".title-3Kf9MY")[1]
+p doc.css(".title-3Kf9MY")[1].name
 
 # courses.each do |course|
 #   puts course.text.strip
